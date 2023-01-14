99acdf23a86eb4cdca67e67ddfa2d0d34c6bc866
Shiloh Olotu <emaildump0305@gmail.com>
2023-01-14 16:11:50 -0600

    Make minor style changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
 let earthParallaxScene = new ScrollParallax();
+
 earthParallaxScene.enable("parallaxEarth",0.5);
+earthParallaxScene.enable("sloganHeader",0.2);
+earthParallaxScene.enable("sloganSub",0.3);
