c7b5edfaabbe03dd19ec61a3700554a690e685c4
Shiloh Olotu <emaildump0305@gmail.com>
2023-01-15 22:32:28 -0600

    Make minor style changes to landing page and add join button

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,4 @@ let earthParallaxScene = new ScrollParallax();
 earthParallaxScene.enable("parallaxEarth",0.5);
 earthParallaxScene.enable("sloganHeader",0.2);
 earthParallaxScene.enable("sloganSub",0.3);
+earthParallaxScene.enable("joinButton",0.4);
